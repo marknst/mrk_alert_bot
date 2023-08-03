42ecdb333fa4c1528cc1ad5d51da3414d93f63fc
Mark Nesterenko <marxxnest@gmail.com>
2023-08-03 18:25:24 +0300

    new module created - eng_words_module, 'random word and context to it' logic in bot implemented

--- bot.py
+++ bot.py
@@ -3,11 +3,21 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 import asyncio
 import config
+from eng_words_module import word_and_context
 
 bot = Bot(token=config.token)
 dp = Dispatcher(bot)
 
-#TODO Bot commands, bot logic modules
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply('Use /start /random /autocheck ')
+
+
+@dp.message_handler(commands=['random'])
+async def send_random_word_and_context(message: types.Message):
+    chat_id = message.from_user.id
+    await bot.send_message(chat_id=chat_id, text=word_and_context.get_word_and_context())
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
